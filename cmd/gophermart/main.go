56b2dd7f3288b9b2f50a763fc8236eb9d08a2dec
AlekseyKas <gos7Pride@yandex.ru>
2022-06-07 08:57:24 +0300

    ty

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,51 +1,58 @@
 package main
 
 import (
-	"context"
 	"net/http"
 	"sync"
 
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
-	"github.com/sirupsen/logrus"
-
-	"github.com/AlekseyKas/gophermart/cmd/gophermart/handlers"
-	"github.com/AlekseyKas/gophermart/cmd/gophermart/storage"
-	"github.com/AlekseyKas/gophermart/internal/app"
-	"github.com/AlekseyKas/gophermart/internal/config"
 )
 
 func main() {
+	// wg := &sync.WaitGroup{}
+	// ctx, cancel := context.WithCancel(context.Background())
+	// err := config.TerminateFlags()
+	// if err != nil {
+	// 	logrus.Error("Error setting args: ", err)
+	// }
+	// // logrus.Info(">>>>>>>>>>>>>", config.Arg.Address, "<<<<<<<<<<<<<<<<<<<<<<")
+
+	// storage.IDB = &storage.DB
+	// storage.IDB.InitDB(ctx, config.Arg.DatabaseURL)
+	// wg.Add(2)
+	// go app.WaitSignals(cancel, wg)
+
+	// r := chi.NewRouter()
+	// b := handlers.NewArgs(r, wg, ctx)
+	// s := &http.Server{
+	// 	Handler: r,
+	// 	Addr:    config.Arg.Address,
+	// }
+	// r.Route("/", b.Router)
+
+	// go func(wg *sync.WaitGroup) {
+	// 	defer wg.Done()
+	// 	err := s.ListenAndServe()
+	// 	if err != nil && err != http.ErrServerClosed {
+	// 		logrus.Error(err)
+	// 	}
+	// }(wg)
+
+	// <-ctx.Done()
+	// s.Shutdown(ctx)
+	// logrus.Info("Stop http server!")
+	// wg.Wait()
 	wg := &sync.WaitGroup{}
-	ctx, cancel := context.WithCancel(context.Background())
-	err := config.TerminateFlags()
-	if err != nil {
-		logrus.Error("Error setting args: ", err)
-	}
-	// logrus.Info(">>>>>>>>>>>>>", config.Arg.Address, "<<<<<<<<<<<<<<<<<<<<<<")
-
-	storage.IDB = &storage.DB
-	storage.IDB.InitDB(ctx, config.Arg.DatabaseURL)
-	wg.Add(2)
-	go app.WaitSignals(cancel, wg)
-
 	r := chi.NewRouter()
-	b := handlers.NewArgs(r, wg, ctx)
-	s := &http.Server{
-		Handler: r,
-		Addr:    config.Arg.Address,
-	}
-	r.Route("/", b.Router)
-
-	go func(wg *sync.WaitGroup) {
-		defer wg.Done()
-		err := s.ListenAndServe()
-		if err != nil && err != http.ErrServerClosed {
-			logrus.Error(err)
-		}
-	}(wg)
-
-	<-ctx.Done()
-	s.Shutdown(ctx)
-	logrus.Info("Stop http server!")
+	r.Route("/", Router)
+	wg.Add(1)
+	go http.ListenAndServe("127.0.0.1:8080", r)
 	wg.Wait()
+}
+
+func Router(r chi.Router) {
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
 }
