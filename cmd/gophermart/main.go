12bed47ef24812efa0b4e33c20fe5056fe76b998
AlekseyKas <gos7Pride@yandex.ru>
2022-06-07 10:50:08 +0300

    sd3

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,28 +1,25 @@
 package main
 
 import (
-	"context"
 	"net/http"
 
-	"github.com/AlekseyKas/gophermart/cmd/gophermart/storage"
-	"github.com/AlekseyKas/gophermart/internal/config"
+	"github.com/AlekseyKas/gophermart/cmd/gophermart/handlers"
 	"github.com/AlekseyKas/gophermart/internal/middlewarecustom"
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
-	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	// wg := &sync.WaitGroup{}
-	ctx, _ := context.WithCancel(context.Background())
-	err := config.TerminateFlags()
-	if err != nil {
-		logrus.Error("Error setting args: ", err)
-	}
-	// logrus.Info(">>>>>>>>>>>>>", config.Arg.Address, "<<<<<<<<<<<<<<<<<<<<<<")
+	// ctx, cancel := context.WithCancel(context.Background())
+	// err := config.TerminateFlags()
+	// if err != nil {
+	// 	logrus.Error("Error setting args: ", err)
+	// }
+	// // logrus.Info(">>>>>>>>>>>>>", config.Arg.Address, "<<<<<<<<<<<<<<<<<<<<<<")
 
-	storage.IDB = &storage.DB
-	storage.IDB.InitDB(ctx, config.Arg.DatabaseURL)
+	// storage.IDB = &storage.DB
+	// storage.IDB.InitDB(ctx, config.Arg.DatabaseURL)
 	// wg.Add(1)
 	// go app.WaitSignals(cancel, wg)
 
@@ -33,19 +30,18 @@ func main() {
 	// 	// Addr:    "127.0.0.1:8080",
 	// 	Addr: config.Arg.Address,
 	// }
-	r.Route("/", Router)
+	r.Route("/", handlers.Router)
 	// wg.Add(1)
 	// go func(wg *sync.WaitGroup) {
 	// 	defer wg.Done()
+	// 	logrus.Info("llllllllllllll")
 	// 	err := http.ListenAndServe("127.0.0.1:8080", r)
 	// 	if err != nil && err != http.ErrServerClosed {
 	// 		logrus.Error(err)
 	// 	}
 	// }(wg)
-
 	http.ListenAndServe("127.0.0.1:8080", r)
 	// <-ctx.Done()
-	// s.Shutdown(ctx)
 	// logrus.Info("Stop http server!")
 	// wg.Wait()
 }
