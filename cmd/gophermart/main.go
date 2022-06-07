078eba73b246e70a4bf66cc5dcee9ef8b06b53ff
AlekseyKas <gos7Pride@yandex.ru>
2022-06-07 09:33:09 +0300

    fix2

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -5,10 +5,11 @@ import (
 	"net/http"
 	"sync"
 
-	"github.com/AlekseyKas/gophermart/cmd/gophermart/handlers"
 	"github.com/AlekseyKas/gophermart/cmd/gophermart/storage"
 	"github.com/AlekseyKas/gophermart/internal/app"
 	"github.com/AlekseyKas/gophermart/internal/config"
+	"github.com/AlekseyKas/gophermart/internal/middlewarecustom"
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 	"github.com/sirupsen/logrus"
 )
@@ -28,13 +29,13 @@ func main() {
 	go app.WaitSignals(cancel, wg)
 
 	r := chi.NewRouter()
-	b := handlers.NewArgs(r, wg, ctx)
+	// b := handlers.NewArgs(r, wg, ctx)
 	s := &http.Server{
 		Handler: r,
 		// Addr:    "127.0.0.1:8080",
 		Addr: config.Arg.Address,
 	}
-	r.Route("/", b.Router)
+	r.Route("/", Router)
 
 	go func(wg *sync.WaitGroup) {
 		defer wg.Done()
@@ -48,4 +49,12 @@ func main() {
 	s.Shutdown(ctx)
 	logrus.Info("Stop http server!")
 	wg.Wait()
+}
+func Router(r chi.Router) {
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(middlewarecustom.CheckCookie)
 }
