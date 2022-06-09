53d0792fd52911829b243e970276f7a0e12a07cd
AlekseyKas <gos7Pride@yandex.ru>
2022-06-09 12:50:53 +0300

    http-accrual

--- internal/config/config.go
+++ internal/config/config.go
@@ -39,7 +39,7 @@ func TerminateFlags() (err error) {
 	var Flags Args
 	flag.StringVar(&Flags.Address, "a", "127.0.0.1:8080", "Address of server")
 	flag.StringVar(&Flags.DatabaseURL, "d", "postgres://user:user@127.0.0.1:5432/db", "Database URL")
-	flag.StringVar(&Flags.SystemAddress, "r", "127.0.0.1:8090", "Address of system accrual")
+	flag.StringVar(&Flags.SystemAddress, "r", "http://127.0.0.1:8090", "Address of system accrual")
 	flag.Parse()
 	logrus.Info("[[[[[[[[[[[[[[[ ENV", env)
 	logrus.Info("5555555555555555555555555555[[[[[[[[[[[[[[[ FLAGS", &Flags)
